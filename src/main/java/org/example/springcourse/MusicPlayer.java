f9b0cecd1fea7c009159ea3dc3e6dfc237ea201d
HououinKyoma2000 <oybek2002olimov@gmail.com>
2022-03-21 19:07:20 +0500

    Внедрение зависимостей. Dependency Injection . Работа с properties.

--- src/main/java/org/example/springcourse/MusicPlayer.java
+++ src/main/java/org/example/springcourse/MusicPlayer.java
@@ -3,10 +3,38 @@ package org.example.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private  String volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public MusicPlayer() {
+    }
+
+    //IoC
     public MusicPlayer(Music music) {
         this.music=music;
     }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
     public void playMusic(){
-        System.out.print("Playing " + music.getSong());
+        System.out.println("Playing " + music.getSong());
     }
 }
