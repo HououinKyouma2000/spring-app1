cb87c8bc88b4523abc539bcd44782b1613f96ad2
HououinKyoma2000 <oybek2002olimov@gmail.com>
2022-03-25 01:10:47 +0500

    Аннотация @Qualifier. Внедрение зависимостей (Dependency Injection).

--- src/main/java/org/example/springcourse/MusicPlayer.java
+++ src/main/java/org/example/springcourse/MusicPlayer.java
@@ -1,37 +1,26 @@
 package org.example.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-/*  v3
+
+/*v1
+  @Autowired
+    @Qualifier("rockMusic")
+    private Music music;*/
+
+    private Music music1;
+    private Music music2;
     @Autowired
-    private Music music;
-*/
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-    @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,@Qualifier("classicalMusic") Music music2) {
+        this.music1=music1;
+        this.music2=music2;
     }
 
-    /*  v1
-    @Autowired
-    public MusicPlayer(Music music) {
-        this.music=music;
-    }*/
- /* V2
-    public MusicPlayer(Music music) {
-    this.music=music;
-}
-    @Autowired
-    public void setMusic(Music music) {
-        this.music=music;
-    }*/
-
     public String playMusic() {
-       return "Playing " + classicalMusic.getSong();
+       return "Playing " + music1.getSong() + " "+music2.getSong();
     }
 }
--- src/main/java/org/example/springcourse/TestSpring.java
+++ src/main/java/org/example/springcourse/TestSpring.java
@@ -9,18 +9,7 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-/*        Music music=context.getBean("rock",Music.class);
 
-        MusicPlayer musicPlayer=new MusicPlayer(music);
-
-        musicPlayer.playMusic();
-
-        Music music1=context.getBean("classical",Music.class);
-        MusicPlayer classicalPlayer =new MusicPlayer(music1);
-        classicalPlayer.playMusic();*/
-
-/*        MusicPlayer musicPlayer =context.getBean("musicPlayer",MusicPlayer.class);
-        musicPlayer.playMusic();*/
 
         Computer computer=context.getBean("computer",Computer.class);
         System.out.println(computer);
