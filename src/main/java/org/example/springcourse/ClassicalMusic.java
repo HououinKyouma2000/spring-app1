096b25c33bf312e3522d9500c142e5f0962cc017
HououinKyoma2000 <oybek2002olimov@gmail.com>
2022-03-25 01:28:29 +0500

    Аннотации @Scope, @Value, @PostConstruct, @PreDestroy.

--- src/main/java/org/example/springcourse/ClassicalMusic.java
+++ src/main/java/org/example/springcourse/ClassicalMusic.java
@@ -1,8 +1,24 @@
 package org.example.springcourse;
 
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/*@Scope("prototype")*/
+/*@Scope("singleton")*/
 @Component
 public class ClassicalMusic implements Music{
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my init");
+    }
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Destroy");
+    }
+
+
     @Override
     public String getSong() {
         return "Mozart";
